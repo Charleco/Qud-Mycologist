ecbc3deee75ea9a0430a9e26778c4377ec5e8586
Charleco <115500158+Charleco@users.noreply.github.com>
2023-08-19 21:25:28 -0400

    Add files via upload

---
+++ Mycologist/MycologistStart.cs
@@ -0,0 +1,18 @@
+using XRL;
+using XRL.World;
+using XRL.World.Effects;
+
+[PlayerMutator]
+public class FungiPlayerMutator : IPlayerMutator
+{
+    public void mutate(GameObject player)
+    {
+        if((player.GetSubtype()).Equals("Mycologist"))
+        {
+        FungalSporeInfection.ApplyFungalInfection(player, "WaxInfection");
+        FungalSporeInfection.ApplyFungalInfection(player, "LuminousInfection");
+        FungalSporeInfection.ApplyFungalInfection(player, "MumblesInfection");
+        FungalSporeInfection.ApplyFungalInfection(player, "PuffInfection");
+        }
+    }
+}
